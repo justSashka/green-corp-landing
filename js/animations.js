ccf3fc235a036751f1e969dc689315b407c76664
justSashka <swaggshka1488@gmail.com>
2022-10-27 10:35:24 +0300

    add anchors and scrolling

--- js/animations.js
+++ js/animations.js
@@ -42,3 +42,36 @@ document.querySelector('#budget').addEventListener('change', function handleSele
     document.querySelector('#form form').removeChild(otherInput);
   }
 });
+
+function addSmoothScroll(anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  addSmoothScroll(anchor);
+});
+
+function updateScroll() {
+  if (window.scrollY > 0) {
+    document.querySelector('header').classList.add('header__scrolled');
+  } else {
+    document.querySelector('header').classList.remove('header__scrolled');
+  }
+
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+  if (windowBottomPosition >= countElementPosition && !animationInited) {
+    animationInited = true;
+    initIncreaseNumberAnimation();
+  }
+}
+
+window.addEventListener('scroll', updateScroll);
+
+addSmoothScroll(document.querySelector('.more-button'));
